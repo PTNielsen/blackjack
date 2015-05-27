7b0370c2bd53796e102652b990a0cf66ea58e683
Patrick Nielsen <ptnielsen55@gmail.com>
2015-05-27 15:22:36 -0400

    Completed Deck Method

--- blackjack.rb
+++ blackjack.rb
@@ -1,10 +1,6 @@
 require "./blackjack_test"
 require "pry"
 
-# class Blackjack
-  
-# end
-
 class Card
 
     def initialize(values, suit)
@@ -17,7 +13,7 @@ class Card
         if @values == :K || @values == :Q || @values == :J
             10
         elsif @values == :A       # Need to make the value :A dependent on what Hand is already equal to
-            11                             # for instance, if Hand > 10; :A == 11    else :A == 1.
+            11                             # for instance, if Hand <= 10; :A == 11    else :A == 1.
         else
             @values.to_i
         end
@@ -30,17 +26,23 @@ class Deck
     def initialize
       @suit_values = [:S, :D, :C, :H]
       @card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+      @full_deck = @card_values.product(@suit_values)
+      @drawn_cards = []
     end
 
     def cards
-          @full_deck = []
-          @full_deck = @card_values.product(@suit_values)
+      return @full_deck
     end
 
     def draw
-      @drawn_card = []
       @shuffled_deck = @full_deck.shuffle!
-      @shuffled_deck.shift(@drawn_card)
+      drawn_card = @shuffled_deck.shift
+      @drawn_cards.push(drawn_card)
+      return drawn_card
+    end
+
+    def drawn
+      return @drawn_cards
     end
 
 end
@@ -52,7 +54,7 @@ class Hand < Card
     end
 
      def add *cards
-
+      # cards.each do
      end
 
     def blackjack?
