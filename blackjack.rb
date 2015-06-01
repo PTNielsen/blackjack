264b3e3d530c9d96e4d3031f3c8aee9598232c47
Patrick Nielsen <ptnielsen55@gmail.com>
2015-05-31 22:15:58 -0400

    Completed Game

--- blackjack.rb
+++ blackjack.rb
@@ -1,4 +1,4 @@
-require "./blackjack_test"
+# require "./blackjack_test"
 require "pry"
 
 class Card
@@ -15,81 +15,86 @@ class Card
   def value
     if @values == :K || @values == :Q || @values == :J
       10
-      elsif @values == :A
-            11
-      else
-          @values.to_i
-      end
+    elsif @values == :A
+      11
+    else
+      @values.to_i
+    end
   end
 
 end
 
-      class Deck 
+class Deck 
 
-        def initialize
-          @suit_values = [:S, :D, :C, :H]
-          @card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
-          @full_deck = @card_values.product(@suit_values)
-          @drawn_cards = []
-        end
-
-        def cards
-          return @full_deck
-        end
-
-        def draw
-          @shuffled_deck = @full_deck.shuffle!
-          drawn_card = @shuffled_deck.shift
-          @drawn_cards.push(drawn_card)
-          return drawn_card
-        end
-
-        def drawn
-          return @drawn_cards
-        end
-
-      end
-
-      class Hand
-
-        def initialize(hand = 0)
-          @my_cards = []
-        end
-
-        def value
-          value_of_cards = 0
-          card_with_ace = false
-          @my_cards.each do | my_card |
-            value_of_cards += my_card.value
-            if my_card.value == 11
-              card_with_ace = true
-            end
-          end
-          if value_of_cards > 21 && card_with_ace
-            value_of_cards -= 10
-          end
-          return value_of_cards
-        end
-
-        def add *cards
-         cards.each do | card |
-          @my_cards.push(card)
-        end
-      end
-
-      def blackjack?
-        value == 21 && @my_cards.count == 2
-      end
-
-      def to_s
-        hand_as_string = []
-        @my_cards.each do | card |
-          hand_as_string.push(card.to_s)
-        end
-        hand_as_string.join(", ")
-      end
-
-      def busted?
-        value  > 21
+  def initialize
+     @suit_values = [:S, :D, :C, :H]
+     @card_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+     @full_deck = []
+     @suit_values.each do |s|
+     @card_values.each do |v|
+        @full_deck.push(Card.new(v, s))
       end
     end
+     @drawn_cards = []
+  end
+
+  def cards
+     return @full_deck
+  end
+
+  def draw
+    @shuffled_deck = @full_deck.shuffle!
+    drawn_card = @shuffled_deck.shift
+    @drawn_cards.push(drawn_card)
+      return drawn_card
+  end
+
+  def drawn
+    return @drawn_cards
+  end
+
+end
+
+class Hand
+
+  def initialize(hand = 0)
+    @my_cards = []
+  end
+
+  def value
+    value_of_cards = 0
+    card_with_ace = false
+    @my_cards.each do | my_card |
+      value_of_cards += my_card.value
+        if my_card.value == 11
+          card_with_ace = true
+        end
+      end
+        if value_of_cards > 21 && card_with_ace
+          value_of_cards -= 10
+        end
+          return value_of_cards
+  end
+
+  def add *cards
+    cards.each do | card |
+      @my_cards.push(card)
+    end
+  end
+
+  def blackjack?
+    value == 21 && @my_cards.count == 2
+  end
+
+  def to_s
+    hand_as_string = []
+    @my_cards.each do | card |
+      hand_as_string.push(card.to_s)
+    end
+      hand_as_string.join(", ")
+  end
+
+  def busted?
+    value  > 21
+  end
+end
---
+++ game.rb
@@ -0,0 +1,104 @@
+require "./blackjack"
+require "pry"
+
+def deal_hands deck, user, dealer
+  2.times { user.add(deck.draw) }
+  puts "Your hand is #{user.to_s} for a value of #{user.value}"
+  2.times { dealer.add(deck.draw) }
+  if user.blackjack?
+    puts "The dealer's hand is #{dealer.to_s} for a value of #{dealer.value}"
+  else
+    puts "The dealer's hand is #{dealer.to_s.slice(0..1)}, '?' for a value of '?'"
+  end
+end
+
+def hit? deck, user, dealer
+  until user.value >= 21
+    print "Would you like to hit? (y/n): "
+    user_input = gets.chomp
+    if user_input == "y"
+      user.add(deck.draw)
+      system "clear"
+      puts "Your hand is #{user.to_s} for a value of #{user.value}"
+    elsif user_input == "n"
+      system "clear"
+      puts "Your hand is #{user.to_s} for a value of #{user.value}"
+      while !dealer.blackjack? &&  !dealer.busted? && dealer.value <  user.value && dealer.value < 17
+      dealer.add(deck.draw)
+      end
+      puts "The dealer's hand is #{dealer.to_s} for a value of #{dealer.value}"
+      break
+    else 
+      puts "Invalid key."
+      hit?(deck, user, dealer)
+    end
+  end
+end
+
+def user_has_won user, dealer
+  if user.blackjack? && !dealer.blackjack?
+    return true
+  elsif user.value > dealer.value && !user.busted?
+    return true
+  elsif !user.busted? && dealer.busted? 
+    return true
+  end
+end
+
+def dealer_has_won user, dealer
+  if dealer.blackjack? && dealer.value > user.value
+    return true
+  elsif user.busted?
+    return true
+  elsif dealer.value > user.value && !dealer.busted?
+    return true
+  end
+end
+
+def result user, dealer
+  if user_has_won(user, dealer)
+    puts "You win!"
+  elsif dealer_has_won(user, dealer)
+    puts "You lose!  Better luck next time!"
+  else
+    puts "It's a push!"
+  end
+end
+
+def play_again? deck, user, dealer
+  print "Would you like to play again? Enter 'y' for 'Yes' or any other key for 'No': "
+  decision = gets.chomp
+  if decision == "y"
+    begin_game(deck, user, dealer)
+  else
+    game_over
+  end
+end
+
+def game_over
+  puts "OK! See you later!"
+end
+
+user = Hand.new
+dealer = Hand.new
+deck = Deck.new
+
+def begin_game deck, user, dealer
+  system "clear"
+  deal_hands(deck, user, dealer)
+  hit?(deck, user, dealer)
+  result(user, dealer)
+  play_again?(deck, user, dealer)
+end
+
+system "clear"
+print "Ready to play Blackjack? Enter 'y' for 'Yes' or any other key for 'No': "
+ready_to_play = gets.chomp
+ready_to_play = "y"
+  if ready_to_play == "y"
+    puts "Good luck!"
+    begin_game(deck, user, dealer)
+  else
+    puts "Maybe next time!"
+    game_over
+  end
